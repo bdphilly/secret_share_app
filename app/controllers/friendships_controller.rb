635d712730a23cf3d2ceaf8a7255051718f00995
Bryan Phillips <bdphilly@gmail.com>
2014-05-09 17:39:54 -0700

    toggle buttons on and off with messages in them

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,26 +1,36 @@
 class FriendshipsController < ApplicationController
   def create
-    sleep(2)
-    new_friendship = Friendship.new(
-      in_friend_id: params[:user_id],
-      out_friend_id: current_user.id
-    )
-
-    if new_friendship.save
-      render json: new_friendship.to_json
-    else
-      render json: 'not okay', status: 422
-
-    end
+    sleep(1)
+    # new_friendship = Friendship.new(
+    #   in_friend_id: params[:user_id],
+    #   out_friend_id: current_user.id
+    # )
+    #
+    # if new_friendship.save
+    #   render json: new_friendship.to_json
+    # else
+    #   render json: 'not okay', status: 422
+    #
+    # end
   end
 
   def destroy
-    sleep(2)
-    no_longer_friends = Friendship.find(params[:id])
+    sleep(1)
 
-    no_longer_friends.delete
-    flash[:errors] = no_longer_friends.errors.full_messages
-    # redirect_to users_url
-    render json: {}
+    # no_longer_friends = Friendship.find_by(
+    #   in_friend_id: params[:friendship][:in_friend_id],
+    #   out_friend_id: current_user.id
+    # )
+    #
+    # no_longer_friends.delete
+    #
+    # flash[:errors] = no_longer_friends.errors.full_messages
+    # render json: {}
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:in_friend_id)
   end
 end
