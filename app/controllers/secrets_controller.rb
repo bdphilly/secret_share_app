fd2f470d511ead893eb554f00a3bf2b11df4b7e0
Bryan Phillips <bdphilly@gmail.com>
2014-05-11 22:57:28 -0700

    fixed Secrets controller. AJAX working.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,17 +1,17 @@
 class SecretsController < ApplicationController
   def new
-    @user = User.find(params[:user_id])
+    @secret = Secret.new
+    @secret.recipient_id = params[:user_id]
+
+    render :new
   end
 
   def create
-    user = User.find(params[:user_id])
-    new_secret = user.received_secrets.new(secret_params)
-    new_secret.author_id = current_user.id
+    @secret = current_user.authored_secrets.new(secret_params)
 
-    if user.save
-      redirect_to user_url(user)
+    if @secret.save
+      render :json => @secret, :head => :ok
     else
-      flash.now[:errors] = user.errors.full_messages
       render :new
     end
   end
@@ -19,6 +19,6 @@ class SecretsController < ApplicationController
   private
 
   def secret_params
-    params.require(:secret).permit(:title, :user_id)
+    params.require(:secret).permit(:title, :recipient_id)
   end
 end
